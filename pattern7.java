d54d1b061722055615dd3f3e97499df7448dc4b6
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-07 09:56:18 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class pattern7 {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(j==0 || i==j || i==n-1)
+                System.out.print("*"+" ");
+            
+            else
+            System.out.print(" "+" ");
+        }
+        
+            
+
+        System.out.println();
+        }
+    }
+}
+        
